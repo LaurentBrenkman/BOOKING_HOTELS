05fa40022bf4b2e741d121a62e03649e3512b7fe
Willy Laurent Brenkman <88841329+LaurentBrenkman@users.noreply.github.com>
2022-07-30 12:46:08 +0700

    HomePage List Component Clear

---
+++ src/components/featuredProperties/FeaturedProperties.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import "./featuredProperties.css"
+
+function FeaturedProperties() {
+  return (
+    <div className='fp'>
+        <div className='fpItem'>
+            <img src='https://cf.bstatic.com/xdata/images/hotel/max1280x900/164162562.jpg?k=93824d71a213e7bb61495983b7e029233f86477b437492dd059e827bd9c87cb3&o=&hp=1' alt='' className='fpImg' />
+                <span className='fpName'>HardRock Hotels</span>
+                <span className='fpCity'>Kuta Bali </span>
+                <span className='fpPrice'>Starting from $200</span>
+                <div className='fpRating'>
+                    <button>9.0</button>
+                    <span>Excellent</span>
+                </div>
+        </div>
+        <div className='fpItem'>
+            <img src='https://cf.bstatic.com/xdata/images/hotel/max1280x900/164162562.jpg?k=93824d71a213e7bb61495983b7e029233f86477b437492dd059e827bd9c87cb3&o=&hp=1' alt='' className='fpImg' />
+            <span className='fpName'>HardRock Hotels</span>
+            <span className='fpCity'>Kuta Bali </span>
+            <span className='fpPrice'>Starting from $200</span>
+            <div className='fpRating'>
+                <button>9.0</button>
+                <span>Excellent</span>
+            </div>
+        </div>
+        <div className='fpItem'>
+            <img src='https://cf.bstatic.com/xdata/images/hotel/max1280x900/164162562.jpg?k=93824d71a213e7bb61495983b7e029233f86477b437492dd059e827bd9c87cb3&o=&hp=1' alt='' className='fpImg' />
+            <span className='fpName'>HardRock Hotels</span>
+            <span className='fpCity'>Kuta Bali </span>
+            <span className='fpPrice'>Starting from $200</span>
+            <div className='fpRating'>
+                <button>9.0</button>
+                <span>Excellent</span>
+            </div>
+        </div>
+        <div className='fpItem'>
+            <img src='https://cf.bstatic.com/xdata/images/hotel/max1280x900/164162562.jpg?k=93824d71a213e7bb61495983b7e029233f86477b437492dd059e827bd9c87cb3&o=&hp=1' alt='' className='fpImg' />
+            <span className='fpName'>HardRock Hotels</span>
+            <span className='fpCity'>Kuta Bali </span>
+            <span className='fpPrice'>Starting from $200</span>
+            <div className='fpRating'>
+                <button>9.0</button>
+                <span>Excellent</span>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default FeaturedProperties
---
+++ src/components/propertyList/PropertyList.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import "./propertyList.css"
+
+function PropertyList() {
+  return (
+    <div className='pList'>
+        <div className='pListItem'>
+            <img src='https://cf.bstatic.com/xdata/images/hotel/max1280x900/369054958.jpg?k=fdb9b1a0a3a8fdc23aac5a090f1082505245b0cd8c8f23c0fef773e6472d3f0f&o=&hp=1' alt='' className='pListImg' />
+        <div className='pListTitles'>
+            <h1>Hotels</h1>
+            <h2>2331 Hotels</h2>
+        </div>
+        </div>
+        <div className='pListItem'>
+            <img src='https://cf.bstatic.com/xdata/images/hotel/max1280x900/369054958.jpg?k=fdb9b1a0a3a8fdc23aac5a090f1082505245b0cd8c8f23c0fef773e6472d3f0f&o=&hp=1' alt='' className='pListImg' />
+        <div className='pListTitles'>
+            <h1>Appartment</h1>
+            <h2>2331 Hotels</h2>
+        </div>
+        </div>
+        <div className='pListItem'>
+            <img src='https://cf.bstatic.com/xdata/images/hotel/max1280x900/369054958.jpg?k=fdb9b1a0a3a8fdc23aac5a090f1082505245b0cd8c8f23c0fef773e6472d3f0f&o=&hp=1' alt='' className='pListImg' />
+        <div className='pListTitles'>
+            <h1>Resorts</h1>
+            <h2>2331 Hotels</h2>
+        </div>
+        </div>
+        <div className='pListItem'>
+            <img src='https://cf.bstatic.com/xdata/images/hotel/max1280x900/369054958.jpg?k=fdb9b1a0a3a8fdc23aac5a090f1082505245b0cd8c8f23c0fef773e6472d3f0f&o=&hp=1' alt='' className='pListImg' />
+        <div className='pListTitles'>
+            <h1>Villas</h1>
+            <h2>2331 Hotels</h2>
+        </div>
+        </div>
+        <div className='pListItem'>
+            <img src='https://cf.bstatic.com/xdata/images/hotel/max1280x900/369054958.jpg?k=fdb9b1a0a3a8fdc23aac5a090f1082505245b0cd8c8f23c0fef773e6472d3f0f&o=&hp=1' alt='' className='pListImg' />
+        <div className='pListTitles'>
+            <h1>Cabins</h1>
+            <h2>2331 Hotels</h2>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default PropertyList
